9052a8925fe137f42531249e32fa86c171c3ce1f
kenshen112 <finalfantasytimothy@gmail.com>
2022-10-10 14:13:40 -0700

    Further implementation of level selector

--- Source/Levels/HubWorld/HubWorld.cs
+++ Source/Levels/HubWorld/HubWorld.cs
@@ -6,6 +6,10 @@ public class HubWorld : LevelCommon
 {
     [Export] List<NodePath> containedLevels;
 
+    Path2D path;
+    PathFollow2D follow2D;
+    Tween interpolate;
+
     LevelSpaces currentSpace;
 
     // Called when the node enters the scene tree for the first time.
@@ -26,6 +30,44 @@ public class HubWorld : LevelCommon
     {
         currentSpace = (LevelSpaces)GetNode(containedLevels[0]);
         player.Position = currentSpace.GlobalPosition;
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        Direction dir = 0;
+        if (Input.IsActionJustPressed("Up"))
+        {
+            dir = Direction.N;
+        }
+
+        if (Input.IsActionJustPressed("Down"))
+        {
+            dir = Direction.S;
+        }
+
+        if (Input.IsActionJustPressed("Left"))
+        {
+            dir = Direction.W;
+        }
+
+        if (Input.IsActionJustPressed("Right"))
+        {
+            dir = Direction.E;
+        }
+
+        if (currentSpace.CanMove(dir))
+        {
+
+            path = (Path2D)GetNode(currentSpace.AttachedPaths[dir]);
+            interpolate = (Tween)path.GetNode("Tween");
+            follow2D = (PathFollow2D)path.GetNode("follow");
+            follow2D.AddChild(player);
+            interpolate.InterpolateProperty(follow2D, "UnitOffset", 0.0f, 1.0f, 3.0f, Tween.TransitionType.Back, Tween.EaseType.InOut);
+
+            // Add player child of next space
+        }
+
+
     }
 
     public override void ExitLevel()
--- Source/Levels/HubWorld/LevelSpaces.cs
+++ Source/Levels/HubWorld/LevelSpaces.cs
@@ -1,19 +1,40 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+
+public enum Direction { N, S, E, W };
 
 public class LevelSpaces : Node2D
 {
     [Export] LevelCommon attachedLevel;
 
+    [Export] private Dictionary<Direction, NodePath> attachedPaths;
+
+    public Dictionary<Direction, NodePath> AttachedPaths
+    {
+        get
+        {
+            return attachedPaths;
+        }
+    }
+
+    Player player;
+
     public void Enter(object body)
     {
         if (body is Player)
         {
+            player = (Player)body;
             if (Input.IsActionJustPressed("Submit"))
             {
-                attachedLevel.EnterLevel((Player)body);
+                attachedLevel.EnterLevel(player);
             }
         }
+    }
+
+    public bool CanMove(Direction dir)
+    {
+        return attachedPaths.ContainsKey(dir);
     }
 
 }
