5021a402778da627f50853a63cb790d5a4b42a3d
kenshen112 <finalfantasytimothy@gmail.com>
2023-01-16 22:22:49 -0700

    Murder weapon system >:D

--- Source/Weapon/WeaponSlot.cs
+++ Source/Weapon/WeaponSlot.cs
@@ -3,53 +3,31 @@ using System;
 
 public class WeaponSlot : Node
 {
-    PackedScene slottedWeapon;
-
-    WeaponCommon active;
-    WeaponCommon current;
-
-    Sprite weaponBox;
-    Sprite weaponIcon;
+    WeaponCommon slottedWeapon;
 
     Player player;
 
     public static Action<Texture> updateWSprite;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public void Equip(WeaponCommon toEquip, Sprite weaponSprite)
     {
-        weaponBox = (Sprite)Owner.GetNode("Camera2D/HUD/WeaponSlot");
-        player = (Player)Owner;
-    }
-
-    public void Equip(PackedScene toEquip, Sprite weaponSprite)
-    {
-        WeaponCommon w = (WeaponCommon)toEquip.Instance();
-
-        if (slottedWeapon != null)
-        {
-            current = (WeaponCommon)slottedWeapon.Instance();
-        }
-        // ToDo - Added equipped weapon into active box
-        if (w != current)
-        {
-            slottedWeapon = toEquip;
-            active = w;
-            updateWSprite.Invoke(weaponSprite.Texture);
-        }
+        slottedWeapon = toEquip;
+        updateWSprite.Invoke(weaponSprite.Texture);
     }
 
     public void Attack()
     {
-        switch (active.weaponType)
+        switch (slottedWeapon.weaponType)
         {
             case WeaponType.THROW:
-                WeaponCommon current = (WeaponCommon)slottedWeapon.Instance();
-                current.Attack(player.direction.Sign());
+                // Needs to make a new instance each time! This spawns in scene when thrown
+                // Does the player need to do this? Or, should this really be in here like it was previously
+                // Even though it was me "Equipping a packed secne" Which won't work for Guns that are attached to player
+                slottedWeapon.Attack(player.direction.Sign());
                 break;
 
             case WeaponType.SHOOT:
-                active.Attack(player.direction.Sign());
+                slottedWeapon.Attack(player.direction.Sign());
                 break;
         }
     }
