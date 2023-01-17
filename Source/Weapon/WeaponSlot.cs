155e53f687aa0f292eacd66e6e2a1d764ae94697
kenshen112 <finalfantasytimothy@gmail.com>
2023-01-16 22:24:38 -0700

    Append Comment

--- Source/Weapon/WeaponSlot.cs
+++ Source/Weapon/WeaponSlot.cs
@@ -23,6 +23,8 @@ public class WeaponSlot : Node
                 // Needs to make a new instance each time! This spawns in scene when thrown
                 // Does the player need to do this? Or, should this really be in here like it was previously
                 // Even though it was me "Equipping a packed secne" Which won't work for Guns that are attached to player
+
+                // WeaponCommon throwable = (WeaponCommon)slottedWeapon.Instance();
                 slottedWeapon.Attack(player.direction.Sign());
                 break;
 
