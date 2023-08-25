b961d73277d4361b6ebdda9c2e2bb9a9048ecf2f
kenshen112 <finalfantasytimothy@gmail.com>
2023-08-24 21:04:38 -0700

    Added plugins

--- Source/Common/TileCommon.cs
+++ Source/Common/TileCommon.cs
@@ -11,11 +11,6 @@ public partial class TileCommon : TileMap
 
     int obj = 0;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
-
     public int Collided(Player Player)
     {
         var pos = LocalToMap(Player.GlobalPosition);
@@ -46,10 +41,5 @@ public partial class TileCommon : TileMap
             scenes.Add(scene.GetSceneTileScene(i));
         }
         return scenes;
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
     }
 }
