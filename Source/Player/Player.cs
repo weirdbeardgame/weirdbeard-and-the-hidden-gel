d6e3182b9a47fba60705bbe44aa0cdc70d224429
ConnorDewald <466872@gmail.com>
2024-08-23 18:56:26 -0500

    Cave Level polished

--- Source/Player/Player.cs
+++ Source/Player/Player.cs
@@ -5,198 +5,198 @@ public enum Objects { NOTHING = 0, SPIKE = 2, LADDER = 1, WATER = 3 }
 
 public partial class Player : Actor
 {
-    private int _Obj;
-    private Camera2D _camera;
-    private Timer _coyoteTimer;
-    //private TileMapLayer _CurrentMap;
-    private Timer _bufferedJumpTimer;
+	private int _Obj;
+	private Camera2D _camera;
+	private Timer _coyoteTimer;
+	//private TileMapLayer _CurrentMap;
+	private Timer _bufferedJumpTimer;
 
-    public int PlayerLives = 3;
-    public Sprite2D WeirdBeard;
-    public WeaponSlot WeaponSlot;
-    public PowerUp CurrentPowerup;
-    public AnimationPlayer AnimationPlayer;
-
-
-    public static Action<PowerUp> OnEquip;
-
-    public void EquipWeapon(PackedScene W, Sprite2D WeaponSprite) => WeaponSlot.SlotWeapon(W, WeaponSprite);
+	public int PlayerLives = 3;
+	public Sprite2D WeirdBeard;
+	public WeaponSlot WeaponSlot;
+	public PowerUp CurrentPowerup;
+	public AnimationPlayer AnimationPlayer;
 
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        base._Ready();
-        WeirdBeard = (Sprite2D)GetNode("CenterContainer/WeirdBeard");
-        AnimationPlayer = (AnimationPlayer)GetNode("AnimationPlayer");
-        _bufferedJumpTimer = (Timer)GetNode("BufferedJump");
+	public static Action<PowerUp> OnEquip;
 
-        _camera = (Camera2D)GetNode("Camera2D");
-        _coyoteTimer = (Timer)GetNode("CoyoteTimer");
+	public void EquipWeapon(PackedScene W, Sprite2D WeaponSprite) => WeaponSlot.SlotWeapon(W, WeaponSprite);
 
-        WeaponSlot = _camera.GetNode<WeaponSlot>("HUD/WeaponSlot");
 
-        SceneManager.StartNewGame += NewGame;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		base._Ready();
+		WeirdBeard = (Sprite2D)GetNode("CenterContainer/WeirdBeard");
+		AnimationPlayer = (AnimationPlayer)GetNode("AnimationPlayer");
+		_bufferedJumpTimer = (Timer)GetNode("BufferedJump");
 
-        GetStateMachine();
+		_camera = (Camera2D)GetNode("Camera2D");
+		_coyoteTimer = (Timer)GetNode("CoyoteTimer");
 
-        Destroyed += GamaOvar;
+		WeaponSlot = _camera.GetNode<WeaponSlot>("HUD/WeaponSlot");
 
-        StateMachine.InitState("IDLE");
-        OnEquip += EquipPowerup;
-        Owner = GetParent();
+		SceneManager.StartNewGame += NewGame;
 
-        if (projectileMotionJump)
-        {
-            JumpVelocity = ((2.0f * jumpHeight) / JumpTimeToPeak) * -1.0f;
-            JumpGravity = ((-2.0f * jumpHeight) / (JumpTimeToPeak * JumpTimeToPeak)) * -1.0f;
-            FallGravity = ((-2.0f * jumpHeight) / (JumpTimeToDescent * JumpTimeToDescent)) * -1.0f;
-        }
+		GetStateMachine();
 
-        ResetPlayer();
-    }
+		Destroyed += GamaOvar;
 
-    public void NewGame()
-    {
-        PlayerLives = 3;
-        ResetPlayer();
-        SceneManager.StartNewGame -= NewGame;
-    }
+		StateMachine.InitState("IDLE");
+		OnEquip += EquipPowerup;
+		Owner = GetParent();
 
-    public void EquipPowerup(PowerUp power)
-    {
-        if (CurrentPowerup != power)
-        {
-            if (StateMachine.Has(power.StateName))
-            {
-                GD.Print("Name: " + power.StateName);
-                StateMachine.RemoveState(CurrentPowerup);
-                RemoveChild(CurrentPowerup);
-            }
-            AddChild(power);
-            StateMachine.AddState(power, power.StateName);
-            CurrentPowerup = power;
-        }
-    }
+		if (projectileMotionJump)
+		{
+			JumpVelocity = ((2.0f * jumpHeight) / JumpTimeToPeak) * -1.0f;
+			JumpGravity = ((-2.0f * jumpHeight) / (JumpTimeToPeak * JumpTimeToPeak)) * -1.0f;
+			FallGravity = ((-2.0f * jumpHeight) / (JumpTimeToDescent * JumpTimeToDescent)) * -1.0f;
+		}
 
-    public void ResetPlayerPosition(Checkpoint CurrentCheckpoint)
-    {
-        Node2D PlayerStartPoint = SceneManager.s_CurrentScene.GetNode<Node2D>("PlayerStartPoint");
+		ResetPlayer();
+	}
 
-        SetPhysicsProcess(false);
+	public void NewGame()
+	{
+		PlayerLives = 3;
+		ResetPlayer();
+		SceneManager.StartNewGame -= NewGame;
+	}
 
-        if (CurrentCheckpoint != null)
-        {
-            Position = CurrentCheckpoint.GlobalPosition;
-        }
-        else
-        {
-            // Need to grab a "Player Starting place"
-            Position = PlayerStartPoint.GlobalPosition;
-        }
-    }
+	public void EquipPowerup(PowerUp power)
+	{
+		if (CurrentPowerup != power)
+		{
+			if (StateMachine.Has(power.StateName))
+			{
+				GD.Print("Name: " + power.StateName);
+				StateMachine.RemoveState(CurrentPowerup);
+				RemoveChild(CurrentPowerup);
+			}
+			AddChild(power);
+			StateMachine.AddState(power, power.StateName);
+			CurrentPowerup = power;
+		}
+	}
 
-    public void ResetPlayer()
-    {
-        ResetActor();
-        SetState("IDLE");
+	public void ResetPlayerPosition(Checkpoint CurrentCheckpoint)
+	{
+		Node2D PlayerStartPoint = SceneManager.s_CurrentScene.GetNode<Node2D>("PlayerStartPoint");
 
-        if (!_camera.Enabled)
-        {
-            ActivateCamera();
-        }
-        SetPhysicsProcess(true);
-    }
+		SetPhysicsProcess(false);
 
-    public void StartCoyoteTimer()
-    {
-        if (_coyoteTimer.IsStopped())
-        {
-            _coyoteTimer.Start(maxCoyoteTimer);
-        }
-    }
+		if (CurrentCheckpoint != null)
+		{
+			Position = CurrentCheckpoint.GlobalPosition;
+		}
+		else
+		{
+			// Need to grab a "Player Starting place"
+			Position = PlayerStartPoint.GlobalPosition;
+		}
+	}
 
-    public void BufferJump()
-    {
-        if (_bufferedJumpTimer.IsStopped())
-        {
-            _bufferedJumpTimer.Start();
-        }
-    }
+	public void ResetPlayer()
+	{
+		ResetActor();
+		SetState("IDLE");
 
-    public void ActivateCamera()
-    {
-        _camera.Enabled = true;
-        _camera.MakeCurrent();
-    }
+		if (!_camera.Enabled)
+		{
+			ActivateCamera();
+		}
+		SetPhysicsProcess(true);
+	}
 
-    public void DeactivateCamera()
-    {
-        _camera.Enabled = false;
-    }
+	public void StartCoyoteTimer()
+	{
+		if (_coyoteTimer.IsStopped())
+		{
+			_coyoteTimer.Start(maxCoyoteTimer);
+		}
+	}
 
-    public void CenterCamera()
-    {
-        Vector2 CameraPosition = _camera.GlobalPosition;
-        CameraPosition.X = GlobalPosition.X;
-        _camera.Position = CameraPosition;
-    }
+	public void BufferJump()
+	{
+		if (_bufferedJumpTimer.IsStopped())
+		{
+			_bufferedJumpTimer.Start();
+		}
+	}
 
-    public bool CanJump => IsOnFloor() || _coyoteTimer.GetTimeLeft() > 0;
+	public void ActivateCamera()
+	{
+		_camera.Enabled = true;
+		_camera.MakeCurrent();
+	}
 
-    public bool CanJumpAgain => IsOnFloor() || NumJumps > 0;
+	public void DeactivateCamera()
+	{
+		_camera.Enabled = false;
+	}
 
-    public void Attack()
-    {
-        WeaponCommon ToUse = WeaponSlot.Weapon.Instantiate<WeaponCommon>();
-        ToUse.Attack(Direction, SceneManager.s_CurrentScene, WeaponUser.PLAYER, this);
-    }
+	public void CenterCamera()
+	{
+		Vector2 CameraPosition = _camera.GlobalPosition;
+		CameraPosition.X = GlobalPosition.X;
+		_camera.Position = CameraPosition;
+	}
 
-    // Game Grumps joke
-    public void GamaOvar()
-    {
-        // Davy Jones stuff in here?
-        // Play Animation
-        ResetPlayer();
-    }
+	public bool CanJump => IsOnFloor() || _coyoteTimer.GetTimeLeft() > 0;
 
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
+	public bool CanJumpAgain => IsOnFloor() || NumJumps > 0;
 
-        //DetectObjects();
+	public void Attack()
+	{
+		WeaponCommon ToUse = WeaponSlot.Weapon.Instantiate<WeaponCommon>();
+		ToUse.Attack(Direction, SceneManager.s_CurrentScene, WeaponUser.PLAYER, this);
+	}
 
-        if (CurrentPowerup != null)
-        {
-            if (CurrentPowerup.CanBeActivated() && Input.IsActionJustPressed("Run"))
-            {
-                CurrentPowerup.Activate();
-            }
-        }
-    }
+	// Game Grumps joke
+	public void GamaOvar()
+	{
+		// Davy Jones stuff in here?
+		// Play Animation
+		ResetPlayer();
+	}
 
-    public void GetAirState()
-    {
-        if (Velocity.Y > 0.0f)
-        {
-            if (StateMachine.CurrentStateName != "FALL")
-            {
-                StateMachine.UpdateState("FALL");
-            }
-        }
-    }
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
 
-    public override void _PhysicsProcess(double delta)
-    {
-        base._PhysicsProcess(delta);
+		//DetectObjects();
 
-        Gravity = GetGravity();
-        if (Input.IsActionJustPressed("Attack") && WeaponSlot.Weapon != null)
-        {
-            Attack();
-        }
+		if (CurrentPowerup != null)
+		{
+			if (CurrentPowerup.CanBeActivated() && Input.IsActionJustPressed("Run"))
+			{
+				CurrentPowerup.Activate();
+			}
+		}
+	}
 
-        ApplyGravity((float)delta);
-        MoveAndSlide();
-    }
+	public void GetAirState()
+	{
+		if (Velocity.Y > 0.0f)
+		{
+			if (StateMachine.CurrentStateName != "FALL")
+			{
+				StateMachine.UpdateState("FALL");
+			}
+		}
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		base._PhysicsProcess(delta);
+
+		Gravity = GetGravity();
+		if (Input.IsActionJustPressed("Attack") && WeaponSlot.Weapon != null)
+		{
+			Attack();
+		}
+
+		ApplyGravity((float)delta);
+		MoveAndSlide();
+	}
 }
